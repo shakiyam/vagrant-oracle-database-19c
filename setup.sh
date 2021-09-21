a311f8efee569db159f5cf90cc2ef3818fd70604
Shinichi Akiyama <shinichi.akiyama@gmail.com>
2021-09-21 12:20:40 +0900

    Follow some best practices for writing shell scripts

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -eu -o pipefail
 
-script_dir="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+readonly SCRIPT_DIR
 
 # load environment variables from .env
 set -a
-if [ -e "$script_dir"/.env ]; then
-  # shellcheck disable=SC1090
-  . "$script_dir"/.env
+if [ -e "$SCRIPT_DIR"/.env ]; then
+  # shellcheck disable=SC1091
+  . "$SCRIPT_DIR"/.env
 else
   echo 'Environment file .env not found. Therefore, dotenv.sample will be used.'
-  # shellcheck disable=SC1090
-  . "$script_dir"/dotenv.sample
+  # shellcheck disable=SC1091
+  . "$SCRIPT_DIR"/dotenv.sample
 fi
 set +a
 
@@ -28,7 +29,7 @@ chown -R oracle:oinstall "$ORACLE_BASE"/..
 chmod -R 775 "$ORACLE_BASE"/..
 
 # Set environment variables
-cat <<EOT >> /home/oracle/.bash_profile
+cat <<EOT >>/home/oracle/.bash_profile
 export ORACLE_BASE=$ORACLE_BASE
 export ORACLE_HOME=$ORACLE_HOME
 export ORACLE_SID=$ORACLE_SID
@@ -37,7 +38,8 @@ EOT
 
 # Install rlwrap and set alias
 # shellcheck disable=SC1091
-readonly OS_VERSION=$(. /etc/os-release; echo "$VERSION")
+OS_VERSION=$(. /etc/os-release && echo "$VERSION")
+readonly OS_VERSION
 case ${OS_VERSION%%.*} in
   7)
     yum -y --enablerepo=ol7_developer_EPEL install rlwrap
@@ -51,22 +53,21 @@ esac
 echo oracle:"$ORACLE_PASSWORD" | chpasswd
 
 # Install database
-/usr/local/bin/mo "$script_dir"/db_install.rsp.mustache >"$script_dir"/db_install.rsp
-su - oracle -c "unzip -d $ORACLE_HOME $script_dir/LINUX.X64_193000_db_home.zip"
+/usr/local/bin/mo "$SCRIPT_DIR"/db_install.rsp.mustache >"$SCRIPT_DIR"/db_install.rsp
+su - oracle -c "unzip -d $ORACLE_HOME $SCRIPT_DIR/LINUX.X64_193000_db_home.zip"
 set +e +o pipefail
 su - oracle -c "cd $ORACLE_HOME && ./runInstaller -silent \
-  -ignorePrereq  -waitforcompletion -responseFile $script_dir/db_install.rsp"
+  -ignorePrereq -waitforcompletion -responseFile $SCRIPT_DIR/db_install.rsp"
 set -e -o pipefail
 "$ORACLE_BASE"/../oraInventory/orainstRoot.sh
 "$ORACLE_HOME"/root.sh
 
 # Create listener using netca
-su - oracle -c "netca -silent -responseFile \
-  $ORACLE_HOME/assistants/netca/netca.rsp"
+su - oracle -c "netca -silent -responseFile $ORACLE_HOME/assistants/netca/netca.rsp"
 
 # Create database
-/usr/local/bin/mo "$script_dir"/dbca.rsp.mustache >"$script_dir"/dbca.rsp
-su - oracle -c "dbca -silent -createDatabase -responseFile $script_dir/dbca.rsp"
+/usr/local/bin/mo "$SCRIPT_DIR"/dbca.rsp.mustache >"$SCRIPT_DIR"/dbca.rsp
+su - oracle -c "dbca -silent -createDatabase -responseFile $SCRIPT_DIR/dbca.rsp"
 
 # Shutdown database
 #echo "shutdown immediate" | su - oracle -c 'sqlplus "/ as sysdba"'
